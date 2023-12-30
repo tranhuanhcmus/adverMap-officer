8ac9125e08d62ab353766e1894175ae9fbe5858f
viet68 <tanviet060801@gmail.com>
2023-12-31 01:57:11 +0700

    Add simple login + app bar

--- advermap-ui/src/App.js
+++ advermap-ui/src/App.js
@@ -1,9 +1,10 @@
 import {BrowserRouter, createBrowserRouter, Outlet, redirect, Router, RouterProvider, Routes} from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import React, {useState} from "react";
-import Login from "./components/login/login.tsx";
+import SignIn from "./components/login/signin.tsx";
 import useToken from "./useToken";
 import {PAGE} from "./components/constants.tsx";
+import AppBar from "./components/appbar/appBar.tsx";
 
 function App() {
   const { token, setToken } = useToken();
@@ -11,6 +12,7 @@ function App() {
   const Layout = () => {
     return (
       <div className="h-screen w-screen bg-white">
+        <AppBar></AppBar>
         <Outlet />
       </div>
     );
@@ -20,7 +22,7 @@ function App() {
   const route = createBrowserRouter([
     {
       path: PAGE.LOGIN,
-      element: <Login  setToken={setToken}/>
+      element: <SignIn  setToken={setToken}/>
     },
     {
       path: "/",
---
+++ advermap-ui/src/components/login/signin.tsx
@@ -0,0 +1,163 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {useNavigate} from "react-router-dom";
+import {useState} from "react";
+import {AuthService} from "../../services/auth/authService.tsx";
+import {AxiosError} from "axios";
+
+function Copyright(props: any) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                Ads management
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export type AuthFieldForm = {
+    username: string;
+    password: string;
+};
+
+const initialFields: AuthFieldForm = {
+    username: '',
+    password: '',
+};
+
+const SignIn = ({setToken} ) => {
+
+    const navigate = useNavigate();
+
+    const [fields, setFields] = useState(initialFields)
+    const [message,setMessage]=useState<string>("")
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault(); // Prevent default form submission behavior
+
+        try {
+            setMessage("Pending")
+            const res = await AuthService.login(fields);
+            if (res.status == 200) {
+                setMessage("Login success ")
+                console.log("Token:" + res.data)
+                setToken(res.data)
+                navigate("/Home", {replace: true})
+            } else {
+                setMessage("Error" + res.message)
+
+            }
+
+        } catch (error) {
+            if (error instanceof AxiosError) {
+                console.log(error);
+
+                setMessage(error.code as string)
+            }
+        }
+        finally{
+            setFields(initialFields)
+        }
+    };
+
+    const handleFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFields({
+            ...fields,
+            [name]: value,
+        });
+    };
+
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box component="form"  id="AuthForm" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="username"
+                            label="Username"
+                            name="username"
+                            autoComplete="email"
+                            value={fields.username}
+                            onChange={handleFieldChange}
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            value={fields.password}
+                            onChange={handleFieldChange}
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+                <Copyright sx={{ mt: 8, mb: 4 }} />
+            </Container>
+        </ThemeProvider>
+    );
+}
+
+export default SignIn;
